113be36a8ca0168a6ca291631fe997524a5c7ba0
rihadatulaisyy <137398414+rihadatulaisyy@users.noreply.github.com>
2023-11-09 10:58:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "BOT-JENNIE"
+global.namaowner = "Jennie"
 global.packname = namabot
 global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
+global.myweb = " "
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6282310493929']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
